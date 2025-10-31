62ec41bb2b3e31b91b54a0bee478dac5b07fe5d9
dutabrahmana <dutalinggau25@gmail.com>
2025-10-31 09:28:42 +0700

    membuat variabel textJK

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -1,16 +1,26 @@
 package com.example.praktikum4
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun FormDataDiri(modifier: Modifier
 ){
     var textNama by remember { mutableStateOf("") }
     var textAlamat by remember {mutableStateOf("")}
+    var textJK by remember {mutableStateOf("")}
 
-}
