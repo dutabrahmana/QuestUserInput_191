8021ea3a46322f253dbba910196910b07e018abd
dutabrahmana <dutalinggau25@gmail.com>
2025-10-31 10:17:28 +0700

    Implementasi pilihan gender menggunakan selectable Row

--- app/src/main/java/com/example/praktikum4/TeksEdit.kt
+++ app/src/main/java/com/example/praktikum4/TeksEdit.kt
@@ -58,4 +58,26 @@ fun FormDataDiri(modifier: Modifier
             }
         )
 
+        Row {
+            gender.forEach { item ->
+                Row(
+                    modifier = Modifier.selectable(
+                        selected = textJK == item,
+
+                        onClick = { textJK = item }
+                    ),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    RadioButton(
+                        selected = textJK == item,
+                        onClick = {
+                            textJK = item
+                        }
+                    )
+                    Text(item)
+                }
+            }
+        }
+
+
        }
